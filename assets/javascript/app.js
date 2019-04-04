5fee19b78a6298d91f8fa734966bae7b011925ab
Louis Rodriguez <louisp285@yahoo.com>
2019-04-04 15:59:49 -0500

    htmlandcss

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+var correctAnswers= []
+
+
+var questions= [
+    "Which artist or band opened the Woodstock Festival in 1969?",
+    "Which European country hosts the annual music festival Tomorrowland?",
+    "Which band realeased the album Abbey Road?",
+    "What is the name of the singer for Red Hot Chili Peppers?",
+    "Which artist hold the record for the most Grammys won?",
+    "Which band produced the theme song for The Big Bang THeory?",
+    "What was the name of Nirvana's debut album?",
+    "Who is the lead singer for Foo Fighters?",
+    
+]
+
+
+
+
+var answerChoices= [[
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
