8532be6480515f8ce5dc3141af03340b9a0b86c4
Louis Rodriguez <louisp285@yahoo.com>
2019-04-07 00:59:36 -0500

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,217 +1,184 @@
-    $(document).ready(function(){ 
-
-    $("#start").on("click", function(){
-      $("#start").remove();
-       game.loadQuestion();
+      $(document).ready(function(){ 
+              // game start, get rid of start button from page, mouse click //
+      $("#start").on("click", function(){
+        $("#start").remove();
+          game.loadQuestion();
+    })
+              // events here, did not know to use(e)!!!!!!!!!!!
+      $(document).on('click','.answer-button',function(e){
+      game.clicked(e);
+    })
+      
+      $(document).on('click', '#reset', function(){
+        game.reset();
     })
     
-    $(document).on('click','.answer-button',function(e){
-    game.clicked(e);
-    })
-    
-    $(document).on('click', '#reset', function(){
-      game.reset();
-    })
-    
-    var questions = [{
-    question: "Which artist or band opened the Woodstock Festival in 1969?",
-    answers: ["Joe Cocker", "Jimi Hendrix", "Richie Havens", "Janis Joplin"],
-    correctAnswer: "Richie Havens",
-    image: "assets/images.gif"
+  var questions = [{
+      question: "Which artist or band opened the Woodstock Festival in 1969?",
+      answers: ["Joe Cocker", "Jimi Hendrix", "Richie Havens", "Janis Joplin"],
+      correctAnswer: "Richie Havens",
+      image: "assets/images.gif"
     }, {
-    
-    question: "Which European country hosts the annual music festival Tomorrowland?",
-    answers: ["Germany", "Belgium","Denmark", "Greece"],
-    correctAnswer: "Belguim",
-    image: "assets/images.gif"
-    },{
-    
-    question: "Which band realeased the album Abbey Road?",
-    answers: ["The Monkees", "The Rolling Stones","Pink Floyd", "The Beatles"],
-    correctAnswer: "The Beatles",
-    image: "assets/images.gif"
-    },{
-
-    question: "What is the name of the lead singer for Red Hot Chili Peppers?", 
-    answers: ["Trent Reznor", "Anthony Kiedis", "Jack White", "Gavin Rossdale"],
-    correctAnswer: "Anthony Kiedis",
-    image: "assets/images.gif"
-    },{
-
-    question: "Which artist holds the record for the most Grammys won?",
-    answers: ["Michael Jackson", "George Solti", "Quincy Jones", "Alison Krauss"],
-    correctAnswer: "George Solti",
-    image: "assets/images.gif"
-    },{
-
-    question: "Which band produced the theme song for The Big Bang Thory?",
-    answers: ["Barenaked Ladies", "Green Day", "Weezer", "Blind Melon"],
-    correctAnswer: "Barenaked Ladies",
-    image: "assets/images.gif"
-    },{
-
-    question: "What was the name of Nirvana's debut album?",
-    answers: ["Volume 1","In Bloom", "Bleach", "Lithium"], 
-    correctAnswer: "Bleach",
-    image: "assets/image.gif"
-    },{
-    
-    question: "What band does David Groel lead sing for?",
-    answers: ["Rage Against the Machine", "Snow Patrol","Imagine Dragons","Foo Fighters"],
-    correctAnswer: "Dave Grohl",
-    image: "assets/image.gif"
-    },{
-      
-    question: "Which latin artist is known as Mr. World Wide?",
-    answers: ["Pitbull", "Julio Iglesias", "Marc Anthony", "Santana"],
-    correctAnswer: "Pitbull",
-    image: "assets/image.gif"
-    },{
-
-    querstion: "Which band headlined for Super Bowl LIII?",
-    answers: ["Justin Timberlake","Travis Scott", "Maroon 5", "Coldplay"],
-    correctAnswer: "Maroon5",
-    image: "assets,image.gif"
-    }];      
-                    
-         function greeting() {
-        startTrivia = $("<button>");
-        startTrivia.addClass("text-center btn btn-warning btn-lg startBtn");
-        startTrivia.text("Start Trivia");
-        $(".main-area").html(startTrivia);
-        console.log(greeting);
-      };
-        greeting();
-    $(".startBtn").on("click", function(event) {
-      gameHTML();
-      timerTrivia();
-    });
-  });
-
-  function triviaTimeOut(){
-    wrongAnswers++; 
-    triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>";
-    $(".main-area").html(triviaHTML);
-    setTimeout(wait, 3000);
-    var loss = $("<p>");
-    loss.addClass("text-center");
-    loss.text("You ran out of time! The correct answer is: " + correctAnswers[currentQuestion]);
-    $(".main-area").append(loss);
-  }
-
-  function triviaWin() {
-    correctAswers++;
-    triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>";
-    $(".main-area").html(triviaHTML);
-    setTimeout(wait,3000);
-    var win = $("<p>");
-    win.addClass("text-center");
-    win.text("Rock On! You are correct: " + correct[currentQuestion]);
-    $(".main-area").append(win);
-    $(".main-area").append(images[currentQuestion]);
-  };
-
-  function triviaLoss() {
-    wrongAnswers++;
-    triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>";
-    $(".main-area").html(triviaHTML);
-    setTimeout(wait,3000);
-    var loss = $("<p>");
-    loss.addClass("text-center");
-    loss.text("WRONG!!! The correct answer is: " + correct[currentQuestion]);
-    $(".main-area").append(loss);
-  };
-
-    function gameHTML(){}
-    triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>";
-    $(".main-area").html(triviaHTML);
-    for (var i=0; i<4; i++) {
-      var answerBtn = $("<button>");
-      answerBtn.addClass("text-center btn btn-warning btn-lg answer");
-      answerBtn.text(choices[currentQuestion][i]);
-      $("main-area").append(answerBtn);
-  }
-
-  $(".answer").on("click", function(event) {
-    selectedAnswer = $(this).text();
-    if (selectedAnswer === correct[currentQuest]) {
-      triviaWin();
-      clearInterval(timer);
-
-    } else {
-      clearInterval(timer);
-      triviaLoss();
-
-    };
-  }); 
-
-  function wait() {
-    if (currentQuestion < 9) {
-        currentQuestion++;
-        gameHTML();
-        counter = 10;
-        timerTrivia();
-    }
-    else {
-        results();
-    }
-};
-
-function timerTrivia() {
-	timer = setInterval(tenSeconds, 10000);
-	function tenSeconds() {
-		if (counter === 0) {
-			clearInterval(timer);
-			triviaTimeOut();
-		}
-		if (counter > 0) {
-			counter--;
-		}
-		$(".timer").html(counter);
-	};
-};
-
-function results() {
-
-		resetTrivia = $("<button>");
-		resetTrivia.addClass("text-center btn btn-warning btn-lg resetBtn");
-		resetTrivia.text("Play Again!");
-		$(".main-area").html(resetTrivia);
-		resetTitle = $("<h2>");
-		resetTitle.addClass("text-center").text("And the results are...");
-		$(".main-area").append(resetTitle);
-		var summaryCorrect = $("<p>");
-		summaryCorrect.addClass("text-center").text("Correct Answers: " + correctAnswers);
-		$(".main-area").append(summaryCorrect);
-		var summaryWrong = $("<p>");
-		summaryWrong.addClass("text-center").text("Wrong Answers: " + wrongAnswers);
-		$(".main-area").append(summaryWrong);
-
-		$(".resetBtn").on("click", function(event) {
-        	window.location.reload();
-    	});	
-};
-
-    results();
-      
   
+      question: "Which European country hosts the annual music festival Tomorrowland?",
+      answers: ["Germany", "Belgium","Denmark", "Greece"],
+      correctAnswer: "Belgium",
+      image: "assets/images.gif"
+    },{
+  
+      question: "Which band realeased the album Abbey Road?",
+      answers: ["The Monkees", "The Rolling Stones","Pink Floyd", "The Beatles"],
+      correctAnswer: "The Beatles",
+      image: "assets/images.gif"
+    },{
 
+      question: "What is the name of the lead singer for Red Hot Chili Peppers?", 
+      answers: ["Trent Reznor", "Anthony Kiedis", "Jack White", "Gavin Rossdale"],
+      correctAnswer: "Anthony Kiedis",
+      image: "assets/images.gif"
+    },{
 
+      question: "Which artist holds the record for the most Grammys won?",
+      answers: ["Michael Jackson", "George Solti", "Quincy Jones", "Alison Krauss"],
+      correctAnswer: "George Solti",
+      image: "assets/images.gif"
+    },{
 
+      question: "Which band produced the theme song for The Big Bang Thory?",
+      answers: ["Barenaked Ladies", "Green Day", "Weezer", "Blind Melon"],
+      correctAnswer: "Barenaked Ladies",
+      image: "assets/images.gif"
+    },{
 
+      question: "What was the name of Nirvana's debut album?",
+      answers: ["Volume 1","In Bloom", "Bleach", "Lithium"], 
+      correctAnswer: "Bleach",
+      image: "assets/image.gif"
+    },{
+      
+      question: "What band does David Grohl lead sing for?",
+      answers: ["Rage Against the Machine", "Snow Patrol","Imagine Dragons","Foo Fighters"],
+      correctAnswer: "Dave Grohl",
+      image: "assets/image.gif"
+    },{
+        
+      question: "Which latin artist is known as Mr. World Wide?",
+      answers: ["Pitbull", "Julio Iglesias", "Marc Anthony", "Santana"],
+      correctAnswer: "Pitbull",
+      image: "assets/image.gif"
+    },{
 
+      question: "Which band headlined for Super Bowl LIII?",
+      answers: ["Justin Timberlake","Travis Scott", "Maroon 5", "Coldplay"],
+      correctAnswer: "Maroon 5",
+      image: "assets,image.gif"
+    }];  
+    
+        //clearing variables for scoring //
+  var game = {
+      questions: questions,
+      currentQuestion:0,
+      counter:10,
+      correct:0,
+      incorrect:0,
+      unanswered:0,
 
+      countdown: function(){
+      game.counter--;
+      $("#counter").text(game.counter);
+      if(game.counter<=0){
+      // console.log(game over!");
+      game.timeUp();
+      }
+    },
 
+      loadQuestion: function(){
+      timer = setInterval(game.countdown,1000);
+      $('#subwrapper').text("Time Remaining <span id='counter'>10</span> Seconds")
+      $('#subwrapper').append('<h2>' + questions[game.currentQuestion].question +'</h2>');
+      for(var i=0; i<questions[game.currentQuestion].answers.length; i++){
+      $('#subwrapper').append('<button class="answer-button" id="button-'+i+'" data-name="'+questions[game.currentQuestion].answers[i] +'">'+questions[game.currentQuestion].answers[i]+'</button>');
+      }
+      },
 
+      nextQuestion: function() {
+      game.counter = 10;
+      $('#counter').text(game.counter);
+      $("#image").text("");
+      game.currentQuestion++;
+      game.loadQuestion();
+    },
 
+      timeUp: function() {
+      clearInterval(timer);
+      game.unanswered++;
+      $('#subwrapper').text('Time is Up!');
+      $('#subwrapper').append('The Correct Answer Is: ' +questions[game.currentQuestion].correctAnswer);
+      $('#image').text("<img src='" + questions[game.currentQuestion].image + "'/>");
+      if(game.currentQuestion==questions.length-1){
+      setTimeout(game.results,2000);
+      } else {
+          setTimeout(game.nextQuestion,2000);
+      }       
+    },
 
+      results: function() {
+      clearInterval(timer);
+      $('#subwrapper').text("Game Over");
+      $('#subwrapper').append("<h3>Correct: "+game.correct +"</h3>");
+      $('#subwrapper').append("<h3>Incorrect: "+game.incorrect +"</h3>");
+      $('#subwrapper').append("<h3>Unanswered: "+game.unanswered+"</h3>");
+      $('#subwrapper').append("<button id='reset'>RESET</button>");
+      $("#image").text("");
+  
+   },
+      clicked: function(e) {
+      clearInterval(timer);
+      if($(e.target).data("name")==questions[game.currentQuestion].correctAnswer){
+      game.answeredCorrectly();
+      } else {
+        game.answeredIncorrectly();
+      }
 
+    },
 
+      answeredCorrectly: function() {
+      console.log("correct");
+      clearInterval(timer);
+      game.correct++;
+      $('#subwrapper').text("Correct, You Rock!!!");
+      $('#image').text("<img src='" + questions[game.currentQuestion].image + "'/>");
+      if(game.currentQuestion==questions.length-1){
+      setTimeout(game.results,2000);
+      } else {
+          setTimeout(game.nextQuestion,2000);
+      }
+    },
 
+      answeredIncorrectly: function() {
+      // console.log("WRONG!");
+      clearInterval(timer);
+      game.incorrect++;
+      $('#subwrapper').text('<h2>YOU GOT IT WRONG!</h2>');
+      $('#subwrapper').append('<h3>The Correct Answer Is: ' +questions[game.currentQuestion].correctAnswer+ '</h3>');
+      $('#image').text("<img src='" + questions[game.currentQuestion].image + "'/>");
+      if(game.currentQuestion==questions.length-1){
+      setTimeout(game.results,2000);
+      } else {
+          setTimeout(game.nextQuestion,2000);
+      }
+    },
 
+      reset: function() {
+      game.currentQuestion = 0;
+      game.counter = 10;
+      game.correct = 0;
+      game.incorrect = 0;
+      game.unanswered = 0;
+      game.loadQuestion();
+      $("#image").text("");
+      }
+    }
+  })
 
-
-
-
-
-
+    
